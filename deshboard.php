644d63a54cacf68f1beabc54ff77a78f6e65948a
mahmudur rahman <mrhridoy397@gmail.com>
2023-09-27 00:46:11 +0600

    dashboard

---
+++ deshboard.php
@@ -0,0 +1,47 @@
+
+<?php
+require_once('./partials/header.php');
+?>
+
+<section class="dashboard">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 pt-5">
+                <div class="card s-bf py-1 rounded-0">
+                    <h5 class="text-light ml-2 mt-2">Total Product</h5>
+                </div>
+                <div class="card mt-5">
+                    <div class="bg-success py-3">
+                        <h1 class="text-center text-light">25</h1>
+                    </div>
+                    <div class="card-header text-center">Monday 25, 2023</div>
+                </div>
+                <div class="card mt-5">
+                    <div class="bg-info py-3">
+                        <h1 class="text-center text-light">0</h1>
+                    </div>
+                    <div class="card-header text-center">INR Total Revenue</div>
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+            <div class="card f-bg py-1 rounded-0">
+                    <h5 class="text-light ml-2 mt-2">Low Stock</h5>
+                </div>
+            </div>
+            <div class="col-md-4 pt-5">
+            <div class="card b-bg py-1 rounded-0">
+                    <h5 class="text-light ml-2 mt-2">Total Orders</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<?php
+require_once('./partials/footer.php');
+?>
---
+++ partials/footer.php
@@ -0,0 +1,15 @@
+
+
+		
+		<!-- Bootstrap v4.5.3 -->
+		<script src="assets/js/bootstrap.min.js"></script>
+		<!-- owl carousel Js -->
+		<script src="assets/js/owl.carousel.min.js"></script>
+		<!-- magic popup js -->
+		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<!-- Main Js -->
+		<!-- <script src="assets/js/sweetalert2.js"></script> -->
+		<script src="assets/js/main.js"></script>
+		
+	</body>
+</html>
